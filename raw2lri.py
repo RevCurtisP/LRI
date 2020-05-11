8db5140c12cf5e7081b0c18601d3d1c44cbe687c
Curtis F Kaylor <revcurtis@gmail.com>
2020-05-11 10:40:13 -0400

    Added raw2lri.py

---
+++ raw2lri.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+#raw2lri.py - Convert Raw Image to LRP image
+
+#Raw Image is extracted from Monochrome 32x32 BMP file
+#Consists of 32 lines of 4 bytes each
+#Top line is bottom of image
+
+def raw2rli(rawdata, invert=False):
+  if len(rawdata) != 128: raise Error("Incorrect Data Length")
+  lridata = []
+  for row in range(124, 0, -8):
+    lriline = ""
+    for index in range (0, 4):
+      upperbyte = rawdata[row + index]
+      lowerbyte = rawdata[row + index - 4]
+      divisor = 64
+      for i in range(0,4):
+        upperbits = int(upperbyte / divisor) & 3
+        lowerbits = int(lowerbyte / divisor) & 3
+        rawchar = upperbits * 4 + lowerbits
+        if invert: rawchar = 15 - rawchar
+        lriline += chr(rawchar + 65)
+        divisor /= 4
+    lridata.append(lriline + '\n')
+  return lridata
+
+def main(imagename, invert=False):
+  rawfilename = imagename+".raw"
+  lrifilename = imagename+".LRI"
+  with open(rawfilename, "rb") as rawfile:
+    rawdata = bytearray(rawfile.read())
+    lridata = raw2rli(rawdata, invert)
+    with open(lrifilename, "w") as lrifile:
+      lrifile.writelines(lridata)
